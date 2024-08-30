1c79c9e650c90290121222388bcba58a7ef9cf00
Meetkumar Chavda <meetkumar.chavda785@gmail.com>
2024-08-30 16:47:26 +0530

    feat:add navbar

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,12 @@
 export const Header = () => {
-  return <div>Header Section</div>;
+  return( 
+  <div className="relative flex itmes-center justify-center top-3">
+    <nav className="flex p-0.5 gap-1 border border-white/15 rounded-full bg-white/10 backdrop-blur">
+      <a href="#" className="nav-item">Home</a>
+      <a href="#" className="nav-item" >Projects</a>
+      <a href="#" className="nav-item" >About</a>
+      <a href="#" className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-grey-900">Contact</a>
+    </nav>
+  </div>
+  )
 };
